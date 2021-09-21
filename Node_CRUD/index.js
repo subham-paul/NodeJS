69e9e31f6e41cb82b19d0a6e059c568ae2dd2d81
Subham Paul <52645265+subham-paul@users.noreply.github.com>
2021-09-22 00:53:53 +0530

    Add files via upload

---
+++ Node_CRUD/index.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+// fs.mkdirSync("subham");// create folder name subham
+
+// fs.writeFileSync("subham/bio.txt", "my name is subham"); // create file write data
+
+// fs.writeFileSync("subham/bio.txt", "my name is subham paul"); // rewrite file data
+
+// fs.appendFileSync("subham/bio.txt", " Backend developer"); // add more data using append
+
+// const data = fs.readFileSync("subham/bio.txt", "utf8");
+// console.log(data); //read file data without buffer data // utf use for encoding purpose
+
+// fs.renameSync("subham/bio.txt", "subham/myBio.txt") // rename file name
+
+// fs.unlinkSync("subham/myBio.txt") // delete file using unlink
+
+// fs.rmdirSync("subham"); //delete subham folder using rmdir
